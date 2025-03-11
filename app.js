2f806e4872289538a69a03441fca334946ebe64f
Frantisek B <frantisekbabinsky.cz@gmail.com>
2025-03-11 01:39:08 +0100

    Iniciacion de RESTful_API

---
+++ app.js
@@ -0,0 +1,18 @@
+// Cargamos el modulo http en el servidor
+const http = require("http");
+// Seleccionamos el puerto 8000 para conflictos con el Front, puerto 3000
+const PORT = 8000;
+// Creamos un servidor http con una función callback que gestione los codigos de respuesta
+const server = http
+  .createServer(
+    // Request contiene los detalles de la solicitud
+    // Response enviará la respuesta al cliente
+    (req, res) => {
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "text/html");
+      res.end("<h1>Hello World!!</h1>");
+    }
+  )
+  .listen(PORT, () => {
+    console.log(`server running at http://localhost:${PORT}`);
+  });
