dabc3296c8535c5cfd73c0508698a5c6c22ea681
Frantisek B <frantisekbabinsky.cz@gmail.com>
2025-03-14 19:59:52 +0100

    all endpoints working

--- app.js
+++ app.js
@@ -1,18 +1,48 @@
-// Cargamos el modulo http en el servidor
-const http = require("http");
-// Seleccionamos el puerto 8000 para conflictos con el Front, puerto 3000
+// Importar los modulos express y mongoose
+const express = require("express");
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", true);
+// Obtener la información el archivo "env"
+require("dotenv").config();
+// Almacenar la cadena de conexión
+const mongoString = process.env.DATABASE_URL;
+console.log(mongoString);
+
+// Conectar con la base de datos
+
+/* Warning: useNewUrlParser is a deprecated option: useNewUrlParser has no effect since Node.js Driver version 4.0.0 and will be removed in the next major version
+(Use `node --trace-warnings ...` to show where the warning was created) */
+/* mongoose.connect(mongoString, { useNewUrlParser: true }); */
+
+mongoose.connect(mongoString);
+
+// Guardar la conexión
+const db = mongoose.connection;
+// Verificar si la conexión ha sido exitosa
+db.on("error", (error) => {
+  console.log(error);
+});
+// Se ejecuta una unica vez, por eso "once" en lugar de "on", cuando se conecta a base de datos, en lugar de en cada petición
+db.once("connected", () => {
+  console.log("succesfully connected");
+});
+// Recibir una notificación cuando la conexión se haya cerrado
+db.on("disconnected", () => {
+  console.log("mongoose default connection is disconnected");
+});
+
+// Importacion de controladores
+const users = require("./Controller/userController");
+
 const PORT = 8000;
-// Creamos un servidor http con una función callback que gestione los codigos de respuesta
-const server = http
-  .createServer(
-    // Request contiene los detalles de la solicitud
-    // Response enviará la respuesta al cliente
-    (req, res) => {
-      res.statusCode = 200;
-      res.setHeader("Content-Type", "text/html");
-      res.end("<h1>Hello World!</h1>");
-    }
-  )
-  .listen(PORT, () => {
-    console.log(`server running at http://localhost:${PORT}`);
-  });
+// Crear la app
+const app = express();
+
+// Analizar los archivos JSON
+app.use(express.json());
+
+app.use("/users", users);
+
+app.listen(PORT, () => {
+  console.log(`server valid al http://127.0.0.1:${PORT}`);
+});
